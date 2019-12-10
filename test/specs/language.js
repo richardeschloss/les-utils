83a5d16ccb18329e452a5d37be94efb2ee922631
Richard Schloss <richard.e.schloss@gmail.com>
2019-12-09 21:46:28 -0700

    Added language utils

---
+++ test/specs/language.js
@@ -0,0 +1,19 @@
+import test from 'ava'
+import { getSupportedLangs, translateText } from '@/utils/language'
+
+test('Translate text', async (t) => {
+  const resp = await translateText({
+    text: 'Hello world!',
+    lang: 'en-es'
+  })
+  const { text, lang } = resp
+  t.is(text[0], 'Hola mundo!')
+  t.is(lang, 'en-es')
+})
+
+test('Supported languages', async (t) => {
+  const resp = await getSupportedLangs({})
+  const { dirs, langs } = resp
+  t.true(dirs.length > 0)
+  t.is(langs.en, 'English')
+})
---
+++ utils/promises.js
@@ -0,0 +1,60 @@
+export function promiseEach(opts) {
+  const { items = [], groupBy, handleItem, transform, notify } = opts
+  const nItems = items.length
+  let doneCnt = 0
+  let out = {}
+  return new Promise((resolve, reject) => {
+    items.forEach(async (item, itemIdx) => {
+      const resp = await handleItem(item, itemIdx).catch(reject)
+      if (groupBy) {
+        out[item[groupBy]] = resp
+      } else if (typeof item === 'string') {
+        out[item] = resp
+      }
+
+      doneCnt++
+      if (notify) {
+        notify({
+          evt: 'promiseEachProgress',
+          data: {
+            progress: doneCnt / nItems,
+            resp,
+            item
+          }
+        })
+      }
+      if (doneCnt === nItems) {
+        if (transform) out = transform(out)
+        resolve(out)
+      }
+    })
+  })
+}
+
+export function promiseSeries(opts) {
+  const { items = [], handleItem, transform, notify } = opts
+  let doneCnt = 0
+  const nItems = items.length
+  let out = {}
+  return new Promise((resolve) => {
+    ;(async function handleNext() {
+      const itemIdx = doneCnt
+      const item = items[itemIdx]
+      const resp = await handleItem(item, itemIdx)
+      out[item] = resp
+      doneCnt++
+      if (notify) notify({ progress: doneCnt / nItems, resp, item })
+      if (doneCnt === nItems) {
+        if (transform) out = transform(out)
+        resolve(out)
+      } else {
+        handleNext()
+      }
+    })()
+  })
+}
+
+export default {
+  promiseEach,
+  promiseSeries
+}
