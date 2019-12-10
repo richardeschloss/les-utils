46da433b6aeaae6744eccda973d9f14fd9f1e243
Richard Schloss <richard.e.schloss@gmail.com>
2019-12-09 23:35:11 -0700

    Added README

--- utils/string.js
+++ utils/string.js
@@ -1,22 +1,22 @@
 import { parseString } from 'xml2js'
 
-export function camelCase (str) {
+export function camelCase(str) {
   return str
-    .replace(/\s(.)/g, function ($1) {
+    .replace(/\s(.)/g, function($1) {
       return $1.toUpperCase()
     })
     .replace(/\s/g, '')
-    .replace(/^(.)/, function ($1) {
+    .replace(/^(.)/, function($1) {
       return $1.toLowerCase()
     })
     .replace(/[^\w\s]/gi, '')
 }
 
-export function startCase (str) {
+export function startCase(str) {
   return str[0].toUpperCase() + str.slice(1)
 }
 
-export function parseXML (xml) {
+export function parseXML(xml) {
   return new Promise((resolve, reject) => {
     parseString(xml, (err, json) => {
       if (err) {
