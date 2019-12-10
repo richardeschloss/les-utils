84d12f554e8e797c11cd2f431ff82e4aa132e436
Richard Schloss <richard.e.schloss@gmail.com>
2019-12-09 22:48:40 -0700

    trying again

--- echoVars.js
+++ echoVars.js
@@ -1,2 +1,14 @@
 console.log('ENV VARS', process.env)
 console.log('argv', process.argv)
+
+if (process.env.K8S_SECRET_VAR2) {
+  console.log('K8S_SECRET_VAR2...it is set!')
+} else {
+  console.log('K8S_SECRET_VAR2 not set...')
+}
+
+if (process.env.SECRET_VAR) {
+  console.log('SECRET_VAR...it is set!')
+} else {
+  console.log('SECRET_VAR not set...')
+}
