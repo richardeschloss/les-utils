c95eead1e872ee08ff1a8b97768d15a9fd947702
Richard Schloss <richard.e.schloss@gmail.com>
2019-12-16 00:31:53 -0700

    Encapsulated code

--- dist/rexters/yandex.js
+++ dist/rexters/yandex.js
@@ -3,7 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.Svc = void 0;
+exports.Svc = Svc;
 
 var _rexter = _interopRequireDefault(require("../rexter"));
 
@@ -13,103 +13,105 @@ const {
   K8S_SECRET_YANDEX_TRANSLATE: YANDEX_API_KEY_BASE64
 } = process.env;
 const reqdVars = ['K8S_SECRET_YANDEX_TRANSLATE'];
-const Svc = Object.freeze({
-  checkEnv() {
-    reqdVars.forEach(v => {
-      if (!process.env[v]) {
-        throw new Error(`${v} undefined. Please define and encode as base64`);
-      }
-    });
-  },
 
-  supportedLangs({
-    ui = 'en'
-  }) {
-    console.log('getting supported langs from Yandex');
-    const postData = {
-      key: YANDEX_API_KEY,
-      ui
-    };
-    return yandexRexter.post({
-      path: '/api/v1.5/tr.json/getLangs',
-      postData,
-      outputFmt: 'json',
-      transform: ({
-        dirs
-      }) => dirs.filter(i => i.startsWith(ui)).map(i => i.split('-')[1])
-    });
-  },
+function checkEnv() {
+  reqdVars.forEach(v => {
+    if (!process.env[v]) {
+      throw new Error(`${v} undefined. Please define and encode as base64`);
+    }
+  });
+}
 
-  translate({
-    text,
-    lang
-  }) {
-    const postData = {
-      key: YANDEX_API_KEY,
+function Svc() {
+  checkEnv();
+  const YANDEX_API_KEY = Buffer.from(YANDEX_API_KEY_BASE64, 'base64').toString();
+  const yandexRexter = (0, _rexter.default)({
+    hostname: 'translate.yandex.net'
+  });
+  return Object.freeze({
+    supportedLangs({
+      ui = 'en'
+    }) {
+      console.log('getting supported langs from Yandex');
+      const postData = {
+        key: YANDEX_API_KEY,
+        ui
+      };
+      return yandexRexter.post({
+        path: '/api/v1.5/tr.json/getLangs',
+        postData,
+        outputFmt: 'json',
+        transform: ({
+          dirs
+        }) => dirs.filter(i => i.startsWith(ui)).map(i => i.split('-')[1])
+      });
+    },
+
+    translate({
       text,
       lang
-    };
-    return yandexRexter.post({
-      path: '/api/v1.5/tr.json/translate',
-      postData,
-      outputFmt: 'json',
-      transform: ({
-        text
-      }) => [{
-        translation: text[0]
-      }]
-    });
-  },
+    }) {
+      const postData = {
+        key: YANDEX_API_KEY,
+        text,
+        lang
+      };
+      return yandexRexter.post({
+        path: '/api/v1.5/tr.json/translate',
+        postData,
+        outputFmt: 'json',
+        transform: ({
+          text
+        }) => [{
+          translation: text[0]
+        }]
+      });
+    },
 
-  async translateMany({
-    texts = [],
-    langs = [],
-    notify,
-    sequential
-  }) {
-    const {
-      supportedLangs
-    } = this;
+    async translateMany({
+      texts = [],
+      langs = [],
+      notify,
+      sequential
+    }) {
+      const {
+        supportedLangs
+      } = this;
 
-    if (langs === 'all') {
-      langs = await supportedLangs({});
+      if (langs === 'all') {
+        langs = await supportedLangs({});
+      }
+
+      console.log('translating for langs', langs);
+      return yandexRexter.requestMany({
+        sequential,
+        items: langs,
+        path: '/api/v1.5/tr.json/translate',
+        postDataTemplate: {
+          key: YANDEX_API_KEY,
+          text: texts,
+          lang: '[ITEM]'
+        },
+        outputFmt: 'json',
+
+        notify({
+          data
+        }) {
+          const {
+            item: lang,
+            resp: result
+          } = data;
+          notify({
+            lang,
+            result
+          });
+        },
+
+        transform: ({
+          text
+        }) => text
+      });
     }
 
-    console.log('translating for langs', langs);
-    return yandexRexter.requestMany({
-      sequential,
-      items: langs,
-      path: '/api/v1.5/tr.json/translate',
-      postDataTemplate: {
-        key: YANDEX_API_KEY,
-        text: texts,
-        lang: '[ITEM]'
-      },
-      outputFmt: 'json',
-
-      notify({
-        data
-      }) {
-        const {
-          item: lang,
-          resp: result
-        } = data;
-        notify({
-          lang,
-          result
-        });
-      },
-
-      transform: ({
-        text
-      }) => text
-    });
-  }
-
-});
-exports.Svc = Svc;
-Svc.checkEnv();
-const YANDEX_API_KEY = Buffer.from(YANDEX_API_KEY_BASE64, 'base64').toString();
-const yandexRexter = (0, _rexter.default)({
-  hostname: 'translate.yandex.net'
-});
+  });
+}
--- src/rexters/ibm.js
+++ src/rexters/ibm.js
@@ -7,102 +7,104 @@ const {
 
 const reqdVars = ['K8S_SECRET_WATSON_TRANSLATE', 'K8S_SECRET_WATSON_ENDPOINT1']
 
-const Svc = Object.freeze({
-  checkEnv() {
-    reqdVars.forEach((v) => {
-      if (!process.env[v]) {
-        throw new Error(`${v} undefined. Please define and encode as base64`)
-      }
-    })
-  },
-  identifiableLanguages() {
-    return ibmRexter.get({
-      path: `/v3/identifiable_languages?version=2018-05-01`,
-      options: {
-        auth: `apikey:${WATSON_API_KEY}`,
-        outputFmt: 'json',
-        transform: ({ languages }) => languages
-      }
-    })
-  },
-  supportedLangs({ src = 'en' }) {
-    console.log('getting supported langs from IBM')
-    return ibmRexter.get({
-      path: `/v3/models?version=2018-05-01`,
-      options: {
-        auth: `apikey:${WATSON_API_KEY}`,
-        outputFmt: 'json',
-        transform: ({ models }) =>
-          models
-            .filter(({ source }) => source === src)
-            .map(({ target }) => target)
-      }
-    })
-  },
-  translate({ text, lang, src = 'en' }) {
-    const postData = {
-      text, // Can be [String] or String
-      model_id: `${src}-${lang}`
+function checkEnv() {
+  reqdVars.forEach((v) => {
+    if (!process.env[v]) {
+      throw new Error(`${v} undefined. Please define and encode as base64`)
     }
-    return ibmRexter
-      .post({
+  })
+}
+
+function Svc() {
+  checkEnv()
+  const WATSON_API_KEY = Buffer.from(WATSON_API_KEY_BASE64, 'base64').toString()
+  const WATSON_ENDPOINT = Buffer.from(
+    WATSON_ENDPOINT1_BASE64,
+    'base64'
+  ).toString()
+
+  const ibmRexter = Rexter({
+    auth: `apikey:${WATSON_API_KEY}`,
+    url: WATSON_ENDPOINT
+  })
+
+  return Object.freeze({
+    identifiableLanguages() {
+      return ibmRexter.get({
+        path: `/v3/identifiable_languages?version=2018-05-01`,
+        options: {
+          auth: `apikey:${WATSON_API_KEY}`,
+          outputFmt: 'json',
+          transform: ({ languages }) => languages
+        }
+      })
+    },
+    supportedLangs({ src = 'en' }) {
+      console.log('getting supported langs from IBM')
+      return ibmRexter.get({
+        path: `/v3/models?version=2018-05-01`,
+        options: {
+          auth: `apikey:${WATSON_API_KEY}`,
+          outputFmt: 'json',
+          transform: ({ models }) =>
+            models
+              .filter(({ source }) => source === src)
+              .map(({ target }) => target)
+        }
+      })
+    },
+    translate({ text, lang, src = 'en' }) {
+      const postData = {
+        text, // Can be [String] or String
+        model_id: `${src}-${lang}`
+      }
+      return ibmRexter
+        .post({
+          path: `/v3/translate?version=2018-05-01`,
+          postData,
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          outputFmt: 'json',
+          transform: ({ translations }) => translations
+        })
+        .catch(() => {
+          throw new Error(`Error translating to lang ${lang}.`)
+        })
+    },
+    async translateMany({
+      texts = [],
+      langs = [],
+      src = 'en',
+      notify,
+      sequential
+    }) {
+      const { supportedLangs } = this
+      if (langs === 'all') {
+        langs = await supportedLangs({})
+      }
+      console.log('translating for langs', langs)
+      return ibmRexter.requestMany({
+        sequential,
+        items: langs,
         path: `/v3/translate?version=2018-05-01`,
-        postData,
+        postDataTemplate: {
+          text: texts,
+          model_id: `${src}-[ITEM]`
+        },
         headers: {
           'Content-Type': 'application/json'
         },
         outputFmt: 'json',
-        transform: ({ translations }) => translations
+        notify({ data }) {
+          const { item: lang, resp: result } = data
+          notify({ lang, result })
+        },
+        transform: ({ translations }) =>
+          translations.map(({ translation }) => translation)
       })
-      .catch(() => {
-        throw new Error(`Error translating to lang ${lang}.`)
-      })
-  },
-  async translateMany({
-    texts = [],
-    langs = [],
-    src = 'en',
-    notify,
-    sequential
-  }) {
-    const { supportedLangs } = this
-    if (langs === 'all') {
-      langs = await supportedLangs({})
     }
-    console.log('translating for langs', langs)
-    return ibmRexter.requestMany({
-      sequential,
-      items: langs,
-      path: `/v3/translate?version=2018-05-01`,
-      postDataTemplate: {
-        text: texts,
-        model_id: `${src}-[ITEM]`
-      },
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      outputFmt: 'json',
-      notify({ data }) {
-        const { item: lang, resp: result } = data
-        notify({ lang, result })
-      },
-      transform: ({ translations }) =>
-        translations.map(({ translation }) => translation)
-    })
-  }
-})
-
-Svc.checkEnv()
-
-const WATSON_API_KEY = Buffer.from(WATSON_API_KEY_BASE64, 'base64').toString()
-const WATSON_ENDPOINT = Buffer.from(
-  WATSON_ENDPOINT1_BASE64,
-  'base64'
-).toString()
-
-const ibmRexter = Rexter({
-  auth: `apikey:${WATSON_API_KEY}`,
-  url: WATSON_ENDPOINT
-})
+  })
+}
 
 export { Svc }
