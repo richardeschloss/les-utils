b92b9b233afee9f5b104b6779b2205773d558899
Richard Schloss <richard.e.schloss@gmail.com>
2019-12-18 14:46:00 -0700

    Added "security" utils

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const { LangUtils } = require('./dist/language')
 const { PromiseUtils } = require('./dist/promise')
 const { default: Rexter } = require('./dist/rexter')
 const { StringUtils } = require('./dist/string')
+const { SecurityUtils } = require('./dist/security')
 
 module.exports = {
   LangUtils,
   PromiseUtils,
   Rexter,
-  StringUtils
+  StringUtils,
+  SecurityUtils
 }
