f2bae5a48d1a5eea14088560faa529024b5cf0d6
Richard Schloss <richardeschloss@gmail.com>
2020-07-19 14:18:59 -0700

    Updated deps

--- unit.config.js
+++ unit.config.js
@@ -1,18 +1,18 @@
-import baseConfig from './ava.config.js'
-
 export default {
-  ...baseConfig,
+  require: ['@babel/register'],
   serial: true,
   files: [
     'test/specs/array.js',
-    // 'test/specs/datetime.js',
-    // 'test/specs/format.js',
-    // 'test/specs/language.js',
-    // 'test/specs/network.js',
-    // 'test/specs/object.js',
-    // 'test/specs/promise.js',
-    // 'test/specs/rexter.js'
-    // 'test/specs/security.js',
-    // 'test/specs/string.js'
-  ]
+    'test/specs/datetime.js',
+    'test/specs/format.js',
+    'test/specs/language.js',
+    'test/specs/network.js',
+    'test/specs/object.js',
+    'test/specs/promise.js',
+    'test/specs/rexter.js',
+    'test/specs/security.js',
+    'test/specs/string.js'
+  ],
+  tap: false,
+  verbose: true
 }
