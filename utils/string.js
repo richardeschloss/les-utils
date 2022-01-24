4e5fc34c8b0d9dced4d151c40e027a7fea291da5
Richard Schloss <richard.e.schloss@gmail.com>
2022-01-24 16:04:55 -0500

    Simplified string utils

--- utils/string.js
+++ utils/string.js
@@ -1,6 +1,5 @@
-import { parseString } from 'xml2js'
+import { parseStringPromise } from 'xml2js'
 
-/** @type {import('./string').stringTostring} */
 export function camelCase (str) {
   return str
     .replace(/\s(.)/g, function($1) {
@@ -13,24 +12,13 @@ export function camelCase (str) {
     .replace(/[^\w\s]/gi, '')
 }
 
-/** @type {import('./string').stringTostring} */
-export const startCase = (str) => 
-  ( str[0].toUpperCase() 
+export function startCase(str) {
+  return ( str[0].toUpperCase() 
   + str.slice(1) ).replace(/([a-z])([A-Z])/g, '$1 $2')
-
-/** @type {import('./string').parseXML} */
-export function parseXML(xml) {
-  return new Promise((resolve, reject) => {
-    parseString(xml, (err, json) => {
-      if (err) {
-        reject(err)
-      } else {
-        resolve(json)
-      }
-    })
-  })
 }
 
-const StringUtils = { camelCase, startCase, parseXML }
+export { parseStringPromise as parseXML }
+
+const StringUtils = { camelCase, startCase, parseXML: parseStringPromise }
 
 export default Object.freeze(StringUtils)
